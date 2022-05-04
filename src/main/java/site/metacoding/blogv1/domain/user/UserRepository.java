ff9a998c10ff3854e16fd818c380e12768b2121d
yuseunghyun <ysh7091@gmail.com>
2022-05-04 21:58:55 +0900

    [blog-V2]11.유저네임 중복검사 확인

--- src/main/java/site/metacoding/blogv1/domain/user/UserRepository.java
+++ src/main/java/site/metacoding/blogv1/domain/user/UserRepository.java
@@ -9,6 +9,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM User WHERE username = :username AND password= :password", nativeQuery = true)
     User mLogin(@Param("username") String username, @Param("password") String password);
 
+    // 중복검사 쿼리문
     @Query(value = "SELECT * FROM User WHERE username =:username", nativeQuery = true)
     User mUsernameSameCheck(@Param("username") String username);
 }
